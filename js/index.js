b211075ca8b174d16b1418247d7e2b4ba1a18c28
unknown <saaluissaka@gmail.com>
2022-08-09 20:37:12 +0000

    Header_Responsive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
+searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+};
+
 window.onscroll = () => {
-  if (window.scrollY > 70) {
+  searchForm.classList.remove("active");
+
+  if (window.scrollY > 80) {
     document.querySelector(".header .header-2").classList.add("active");
   } else {
     document.querySelector(".header .header-2").classList.remove("active");
